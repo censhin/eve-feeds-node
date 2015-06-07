0fbeb191537e79610285cb9b95df0a2e956f3bac
censhin <censhin@gmail.com>
2015-06-07 04:30:34 +0000

    feat(app): created default config and set params on client

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var sio = require('socket.io');
 var hamster = require('hamster');
+var config = required('./config');
+
+hamster.setParams({
+    keyID: config.keyID,
+    vCode: config.vCode
+});
 
 var port = 8081;
 var io = sio(port);
