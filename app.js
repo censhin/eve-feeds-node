47cb022ba9c676446123f1c4b806c18fcf58b1f6
censhin <censhin@gmail.com>
2015-06-11 17:52:42 +0000

    committing changes

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var sio = require('socket.io');
 var hamster = require('hamster');
-var config = required('./config');
+var config = require('./config');
+var redis = require('redis');
+var express = require('express');
+
+var app = express();
 
 hamster.setParams({
     keyID: config.keyID,
@@ -9,4 +13,27 @@ hamster.setParams({
 
 var port = 8081;
 var io = sio(port);
-console.log('listening on *:' + port);
+console.log('socket.io listening on *:' + port);
+
+io.on('connection', function(socket){
+  console.log('a subscriber connected');
+});
+
+function subscribe(req, res, next) {
+    io.on('connection', function(socket){
+      console.log('a subscriber connected');
+    });
+
+    res.send('you have successfully connected');
+    return next();
+}
+
+app.get('/subscribe', subscribe);
+
+var server = app.listen(8080, function() {
+    var host = server.address().address;
+    var port = server.addres().port;
+
+    console.log('api listening on *:' + port)
+});
+
