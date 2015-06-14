8477774b816b362848c04e54018184547e4be4a6
censhin <censhin@gmail.com>
2015-06-14 20:24:24 +0000

    feat(app): there's no way this works

--- app.js
+++ app.js
@@ -1,28 +1,87 @@
+var sleep = require('sleep');
+
+var _ = require('lodash');
+var redis = require('redis');
 var hamster = require('hamster');
+
 var config = require('./config');
+
+redisClient = redis.createClient();
 
 hamster.setParams({
     keyID: config.keyID,
     vCode: config.vCode
 });
 
-hamster.fetch('char:Notifications', {}, function (err, res) {
-    if (err) throw err
-    var notifications = res.notifications;
-    for(var key in notifications) {
-        var typeId = notifications[key].typeID;
-        if(typeId === 75 || typeId === 76) {
-            var cachedNotifications = 'stuff'; // pull data from redis
-            for(var cKey in cachedNotifications) {
-                if(cachedNotifications.Id != key) {
-                    // cache the notification
-                    hamster.fetch('char:NotificationTexts', {IDs: key}, function(err, res) {
-                        if (err) throw err
-                        // publish the notification to all subscribers
-                    }
-                }
-            }
-        }
+var fetchPosNotifications = function(callback) {
+    hamster.fetch('char:Notifications', {}, function (err, res) {
+        if(err) throw err
+        var notifications = res.notifications;
+
+        // filter out all notifications w/o the right ids
+        var posNotifications = _.filter(notifications, function (notification) {
+          return notification.typeID == 75 || notification.typeID == 76;
+        });
+
+        callback(posNotifications);
+    });
+}
+
+var getNewNotificationIds = function(notifications, callback) {
+        // convert that collection an array with just the keys
+        var typeKeys = _.keys(matchingType);
+
+        // get the cached notifications and gets an array of their ids
+        var cachedNotifications = redisClient.hgetall('notifications', function(err, obj) {
+            if(err) throw err
+            return obj
+        });
+
+        var notificationIds = _.pluck(cachedNotifications, 'Id');
+
+        // only keep the notification id's that don't already exist
+        var uniqueIds = _.difference(notificationIds, typeKeys);
+
+        callback(uniqueIds);
+}
+
+var publishNewNotifications = function(uniqueIds, callback) {
+    _.each(uniqueIds, function (id) {
+        hamster.fetch('char:NotificationTexts', {IDs: id}, function(err, res) {
+            if(err) throw err
+            // publish the notification to all subscribers
+        });
+    });
+}
+
+var startup = function() {
+    var resp = fetchPosNotifications(function(err, res) {
+        if(err) throw err
+        return res;
+    });
+
+    redisClient.hmset(notificaitons, resp);
+}
+
+startup();
+
+for() {
+    var resp = fetchPosNotifications(function(err, res) {
+        if(err) throw err
+        return res;
+    });
+
+    var newNotifications = getNewNotificationIds(resp, function(err, res) {
+        if(err) throw err
+        return res;
+    });
+
+    if(newNotifications) {
+        publishNewNotifications(newNotifications, function(err) {
+            if(err) throw err
+        });
     }
-});
+
+    sleep.sleep(1800);
+}
 
